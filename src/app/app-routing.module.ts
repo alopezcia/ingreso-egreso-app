44170e77d7314ce85884bf3d54038e48f46896aa
Adolfo López <alopezcia@gmail.com>
2020-03-08 20:15:33 +0100

    Arreglo lazy-load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { 
-        path: 'ingreso-egreso',
+        path: '',
         loadChildren:  () => import('./ingreso-egreso/ingreso-egreso.module').then(m => m.IngresoEgresoModule),
         canLoad: [ AuthGuardService ]
     },
